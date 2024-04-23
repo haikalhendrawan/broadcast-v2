3c7dde0a7bf105b28e0b3172f36be8985caadc0f
haikalhndrwn <haikalhendrawan88888@gmail.com>
2024-04-23 08:17:01 +0700

    fix client initialization error

--- src/config/client.ts
+++ src/config/client.ts
@@ -2,6 +2,11 @@ import Whatsapp from 'whatsapp-web.js'
 const { Client, LocalAuth } = Whatsapp;
 
 const client = new Client({
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  },
   restartOnAuthFail: true,
   puppeteer: {
     headless: true,
